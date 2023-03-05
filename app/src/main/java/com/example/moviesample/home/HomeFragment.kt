d76730bb1f12453fb2cc25ef889da186a5d0629d
arecalde <arecalde@ucdavis.edu>
2023-03-05 15:56:40 -0800

    Init commit

---
+++ app/src/main/java/com/example/moviesample/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.moviesample.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.moviesample.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentHomeBinding.inflate(inflater)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/moviesample/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.moviesample.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    val fragmentName = "Home Fragment"
+}
